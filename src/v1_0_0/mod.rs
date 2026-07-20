e86002b623e1807997989ae34bc7e043a18623ab
shaugland <279112458+shauglandarka@users.noreply.github.com>
2026-07-20 12:25:54 -0400

    update ndarray to 0.17 and bump version to 0.0.2

--- src/v1_0_0/mod.rs
+++ src/v1_0_0/mod.rs
@@ -1,4 +1,5 @@
 use serde::Deserialize;
+use ndarray::Array1;
 
 pub mod display;
 pub mod exploitation_features;
@@ -128,6 +129,12 @@ pub struct Ecef {
     pub y: f64,
     #[serde(rename = "Z")]
     pub z: f64,
+}
+
+impl Ecef {
+    pub fn to_array(&self) -> Array1<f64> {
+        Array1::from_vec(vec![self.x, self.y, self.z])
+    }
 }
 
 #[derive(Debug, Deserialize, PartialEq, Clone)]
